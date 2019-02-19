9279fd405a92ccafd991af315c5f9466d9489105
Lara <gabriel.lara@externos-ar.cencosud.com>
2019-02-19 13:35:24 -0300

    Martes tarde

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -7,23 +7,23 @@ function mostrar()
     var formapago;
     libros=prompt("Cuantos libros desea?");
     valorlibro=prompt("Valor del libro");
+    formapago = prompt("Desea pagar con tarjeta? si o no");
     descuentouno=parseInt(valorlibro) * 0.9;
     descuentodos=parseInt(valorlibro) * 0.75;
-    formapago = prompt("Desea pagar con tarjeta? si o no");
-    if ((libros>=2 && valorlibro<=2000) && (formapago=="no")) {
+    if ((libros>=2 && valorlibro<2000) && (formapago=="no")) {
         alert("Usted compro " + libros + " Libros y tiene un 10% de descuento $" + descuentouno);        
     }
     else {
-        if ((libros>=2 && valorlibro<=2000) && (formapago=="si")) {
-            alert("Usted compro " + libros + " Libros y tiene un 10% de descuento $" + descuentouno + ", mas el 10% de recargo de recargo por pago con tarjeta $" + descuentouno*1.1);            
+        if ((libros>=2 && valorlibro<2000) && (formapago=="si")) {
+            alert("Usted compro " + libros + " Libros y tiene un 10% de descuento $" + descuentouno + ", mas el 10% de recargo por pago con tarjeta $" + descuentouno*1.1);            
         }
         else {
-            if ((libros>=2 && valorlibro>2000) && (formapago=="no")) {
-                alert("Usted compro " + libros + " Libros y tiene un 15% de descuento " + descuentodos);
+            if ((libros>=2 && valorlibro>=2000) && (formapago=="no")) {
+                alert("Usted compro " + libros + " Libros y tiene un 25% de descuento " + descuentodos);
             }
             else {
-                if ((libros>=2 && valorlibro>2000) && (formapago=="si")) {
-                    alert("Usted compro " + libros + " Libros y tiene un 15% de descuento $" + descuentodos + ", mas el 10% de recargo por pago con tarjeta $" + descuentodos*1.1);                    
+                if ((libros>=2 && valorlibro>=2000) && (formapago=="si")) {
+                    alert("Usted compro " + libros + " Libros y tiene un 25% de descuento $" + descuentodos + ", mas el 10% de recargo por pago con tarjeta $" + descuentodos*1.1);                    
                 }
                 else {
                     if (libros<2 && formapago=="si") {
